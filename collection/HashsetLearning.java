f252b173f5bd0a33943354e4ef799502e1bec493
mohankumaronly <mohankumaronly81@gmail.com>
2026-06-26 13:11:47 +0530

    hash map

--- collection/HashsetLearning.java
+++ collection/HashsetLearning.java
@@ -1,18 +1,31 @@
 package collection;
-
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class HashsetLearning {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        HashSet<Integer> set = new HashSet<Integer>();
-        
+        int n = scanner.nextInt();
 
-        // for (int i = 0; i < mx; i++) {
-            
-        // }
+        TreeSet<Integer> treeSet = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            treeSet.add(scanner.nextInt());
+        }
+
+        treeSet.descendingSet();
+
+        System.out.println(treeSet);
+
+        Collections.reverseOrder();
+
+        System.out.print(treeSet);
+
+
+
 
         scanner.close();
     }
